166ecec931c6edc61c5704f6a38dd59a8233223e
msankrish92 <33427950+msankrish92@users.noreply.github.com>
2020-05-14 00:32:12 +0530

    Add files via upload

---
+++ src/dailyChallenges/SumOfGivenNumbers.java
@@ -0,0 +1,15 @@
+package programs.dailyChallenges;
+
+public class SumOfGivenNumbers {
+
+	
+	
+//	. Write a java code to find the sum of the given numbers
+//	Input: "asdf1qwer9as8d7"
+//	output: 1+9+8+7 = 25
+//	2. Write a java program to do the following.
+//	Input: "When the world realise its own mistake, corona will dissolve automatically"
+//	output: "When eht world esilaer its nwo mistake, anoroc will evlossid automatically"
+//	3. Write a java program to find the number of Uppercase, lowercase, numbers and spaces in the following String.
+//	Input: "1. It is Work from Home  not Work for Home"
+}
